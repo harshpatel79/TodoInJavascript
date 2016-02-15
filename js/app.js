0817eeafc26a4beb275b98511f143d4e238b723e
harshpatel7962 <harshpatel7962@gmail.com>
2016-02-15 19:05:20 +0530

    updated for 15th

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-var myapp = angular.module("myapp",['ngSanitize']);
+var app = {};
+var Todotask = []
+app.myFunction = function(){
+	var task = {};
+    task.title = document.getElementById('ToDoTitle').value;
+	task.Description = document.getElementById('Description').value;
+	task.startTime = document.getElementById('datetimepicker1').value;
+	task.endTime = document.getElementById('datetimepicker2').value;
+	if((task.title == '') || (task.Description == '') || (task.startTime == '') || (task.endTime == '')){
+		alert("Please fill in all fields.");
+	}
+	else{
+		Todotask.push(task);
+		console.log("task",task);
+		console.log("Main task",Todotask);
+		//document.getElementsByClassName("verify").value = "";
+		$(".verify").val('');
+		$('#myModalHorizontal').modal('hide');
+	}
+
+};
+
+
+$( document ).ready(function() {
+	$('#datetimepicker1,#datetimepicker2').datetimepicker();
+});
+
